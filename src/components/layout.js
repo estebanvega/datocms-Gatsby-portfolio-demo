6c2ac8b75c48c29edb54bf08db2977d80eb88941
Esteban Vega <esteban.vega@spp.se>
2019-06-25 12:16:28 +0200

    Move hardcoded markup to index template

--- src/components/layout.js
+++ src/components/layout.js
@@ -50,25 +50,7 @@ const TemplateWrapper = ({ children }) => (
             <link rel="stylesheet" id="sapor-style-css" href="https://www.euroben.se/wp-content/themes/sapor/style.css?ver=5.2.2" type="text/css" media="all"></link>
           </HelmetDatoCms>
 
-          <div id="primary" className="content-area">
-            <main id="main" className="site-main" role="main">
-              <article id="post-747" className="post-747 page type-page status-publish has-post-thumbnail hentry">
-                <div className="entry-wrapper">
-                  <div className="featured-image">
-                    <img width={900} height={564} src="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=900%2C564&ssl=1" className="attachment-sapor-featured-image size-sapor-featured-image wp-post-image" alt srcSet="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=4892&ssl=1 4892w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=300%2C188&ssl=1 300w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=768%2C481&ssl=1 768w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=1024%2C642&ssl=1 1024w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=900%2C564&ssl=1 900w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=260%2C163&ssl=1 260w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=1460&ssl=1 1460w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=2190&ssl=1 2190w" sizes="(max-width: 900px) 100vw, 900px" data-attachment-id={868} data-permalink="https://www.euroben.se/shutterstock_320953616/" data-orig-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=4892%2C3067&ssl=1" data-orig-size="4892,3067" data-comments-opened={0} data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;Shutterstock \/ Leonid Andronov&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;320953616&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="shutterstock_320953616" data-image-description data-medium-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=300%2C188&ssl=1" data-large-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=730%2C458&ssl=1" />	</div>{/* .post-thumbnail */}
-                  <header className="entry-header">
-                  </header>{/* .entry-header */}
-                  <div className="entry-meta">
-                  </div>{/* .entry-meta */}
-                  <div className="entry-content">
-                    <p><strong>Euroben Life &amp; Pension</strong> är ett irländskt livförsäkringsbolag som ingår i Storebrandkoncernen. Bolaget tillhandahåller pensionslösningar i Sverige via sin <span style={{ color: '#003366' }}><a style={{ color: '#003366' }} href="https://www.euroben.se//kontakta-oss/">svenska filial</a></span>.</p>
-                  </div>{/* .entry-content */}
-                  <footer className="entry-footer">
-                  </footer>{/* .entry-footer */}
-                </div>
-              </article>{/* #post-## */}
-            </main>{/* #main */}
-          </div>
+          {children}
 
           <div id="secondary" className="sidebar" role="complementary">
             <a href="https://www.euroben.se/" className="site-logo-link" rel="home" itemProp="url"><img width={260} height={57} src="https://i0.wp.com/www.euroben.se/wp-content/uploads/2017/04/euroben_logo_rgb_png-e1491480674577.png?fit=260%2C57&ssl=1" className="site-logo attachment-sapor-logo" alt data-size="sapor-logo" itemProp="logo" data-attachment-id={963} data-permalink="https://www.euroben.se/euroben_logo_rgb_png/" data-orig-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2017/04/euroben_logo_rgb_png-e1491480674577.png?fit=260%2C57&ssl=1" data-orig-size="260,57" data-comments-opened={0} data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="euroben_logo_rgb_png" data-image-description data-medium-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2017/04/euroben_logo_rgb_png-e1491480674577.png?fit=300%2C66&ssl=1" data-large-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2017/04/euroben_logo_rgb_png-e1491480674577.png?fit=730%2C161&ssl=1" /></a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,26 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
+      <div id="primary" className="content-area">
+        <main id="main" className="site-main" role="main">
+          <article id="post-747" className="post-747 page type-page status-publish has-post-thumbnail hentry">
+            <div className="entry-wrapper">
+              <div className="featured-image">
+                <img width={900} height={564} src="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=900%2C564&ssl=1" className="attachment-sapor-featured-image size-sapor-featured-image wp-post-image" alt srcSet="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=4892&ssl=1 4892w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=300%2C188&ssl=1 300w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=768%2C481&ssl=1 768w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=1024%2C642&ssl=1 1024w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=900%2C564&ssl=1 900w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?resize=260%2C163&ssl=1 260w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=1460&ssl=1 1460w, https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?w=2190&ssl=1 2190w" sizes="(max-width: 900px) 100vw, 900px" data-attachment-id={868} data-permalink="https://www.euroben.se/shutterstock_320953616/" data-orig-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=4892%2C3067&ssl=1" data-orig-size="4892,3067" data-comments-opened={0} data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;Shutterstock \/ Leonid Andronov&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;320953616&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="shutterstock_320953616" data-image-description data-medium-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=300%2C188&ssl=1" data-large-file="https://i0.wp.com/www.euroben.se/wp-content/uploads/2016/12/shutterstock_320953616.jpg?fit=730%2C458&ssl=1" />	</div>{/* .post-thumbnail */}
+              <header className="entry-header">
+              </header>{/* .entry-header */}
+              <div className="entry-meta">
+              </div>{/* .entry-meta */}
+              <div className="entry-content"
+                dangerouslySetInnerHTML={{
+                  __html: data.home.introTextNode.childMarkdownRemark.html,
+                }} />
+              <footer className="entry-footer">
+              </footer>{/* .entry-footer */}
+            </div>
+          </article>{/* #post-## */}
+        </main>{/* #main */}
+      </div>
     </Layout>
   );
 }
@@ -19,21 +39,13 @@ const IndexPage = ({ data }) => {
 export default IndexPage
 
 export const query = graphql`
-  query IndexQuery {
-    allDatoCmsWork(sort: { fields: [position], order: ASC }) {
-      edges {
-        node {
-          id
-          title
-          slug
-          excerpt
-          coverImage {
-            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
-              ...GatsbyDatoCmsSizes
-            }
-          }
-        }
+query IndexQuery {
+  home: datoCmsHome {
+    introTextNode {
+      childMarkdownRemark {
+        html
       }
     }
   }
+}
 `
